1b3e8ee39367d7f5991f4b63a4c26ff6145160b7
Rajnesh <rajnesh34698@gmail.com>
2024-06-11 21:59:15 +0530

    removed errors

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ main()
 
 async function main() {
 //   await mongoose.connect('mongodb://127.0.0.1:27017/fmc');
-  await mongoose.connect(dburl);
+  await mongoose.connect("mongodb://"+dburl);
 };
 
 
